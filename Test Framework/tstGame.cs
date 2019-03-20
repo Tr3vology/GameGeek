b344c9d4f1f24fd3a4f8d2fe380615f31b6aa910
Tr3vology <P17200124@my365.dmu.ac.uk>
2019-03-20 12:23:22 +0000

    Creation of ClsGame + test class

--- Test Framework/tstGame.cs
+++ Test Framework/tstGame.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using Class_Library;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test_Framework
 {
     [TestClass]
-    public class UnitTest1
+    public class tstGame
     {
         [TestMethod]
         public void TestMethod1()
         {
+            //create an instance of our class clsGame
+            clsGame AGame = new clsGame();
+            Assert.IsNotNull(AGame);
         }
     }
 }
