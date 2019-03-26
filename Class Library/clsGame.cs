25fd862de9fe235c46477f1cdf113f7571113a29
Tr3vology <P17200124@my365.dmu.ac.uk>
2019-03-26 11:18:11 +0000

    Adding detail to clsGame

--- Class Library/clsGame.cs
+++ Class Library/clsGame.cs
@@ -1,6 +1,25 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsGame
     {
+        public string GameName { get; set; }
+        public double GamePrice { get; set; }
+
+        public bool Find(string someGame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(string someGame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Valid(double someGamePrice)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
